b2df1f3269a6a4de746f2ee22eb67bed9c0f7709
lbellan1 <lbellan1@ford.com>
2016-06-01 21:13:39 -0400

    June 1 update part 5 - added back in AMP support that got lost due to git BS

--- ConfigFile.py
+++ ConfigFile.py
@@ -21,11 +21,11 @@ config_list2 = []  # master list of dictionary pairs - filename, displayname
 config_list2.append({"filename": "oobd_control_C346.ini", "displayname": "C346"})  # index = 0 NEED TO TEST STILL!!!! 125K ms, visteon AHU no tweets, ipc/bcm
 config_list2.append({"filename": "oobd_control_S550.ini", "displayname": "S550"})  # index = 1 #use ABS for VIN  500k ms, clarion, sp=2
 config_list2.append({"filename": "oobd_control_B299.ini", "displayname": "B299"})  # index = 2 #use apim for VIN this worked in B299N  IPC works too.  hs 500k, visteon, sp=1
-config_list2.append({"filename": "oobd_control_CD390.ini", "displayname": "CD390"})  # index = 3 NEED TO TEST STILL!!!! TODO-test CD390 Fusion vehicle
+config_list2.append({"filename": "oobd_control_CD390.ini", "displayname": "CD390"})  # index = 3 NEED TO TEST STILL!!!! TODO-test CD390 Fusion vehicle again based on EU issues
 config_list2.append({"filename": "oobd_control_CD391.ini", "displayname": "CD391"})  # index = 4 ms 500k, Panasonic + tweeters, sub, VIN=rcm + abs
 config_list2.append({"filename": "oobd_control_C520.ini", "displayname": "C520"})  # index = 5 use IPC for VIN on C520N or BCM OK ?, ms 125k, panasonic escape
 config_list2.append({"filename": "oobd_control_CD539.ini", "displayname": "CD539"})  # index = 6 ms 500k, visteon, sp=1 edge
-config_list2.append({"filename": "oobd_control_V408.ini", "displayname": "V408-US"})  # index = 7 hs 125k, visteon, sp=1, vin ipc+bcm TODO-test other configurations of V408
+config_list2.append({"filename": "oobd_control_V408.ini", "displayname": "V408-US"})  # index = 7 hs 125k, visteon, sp=2, vin ipc+bcm TODO-test other configurations of V408
 config_list2.append({"filename": "oobd_control_U502.ini", "displayname": "U502"})  # index = 8 use ABS or RCM for VIN  500k ms, clarion, sp=2
 config_list2.append({"filename": "oobd_control_P552.ini", "displayname": "P552"})  # index = 9 # use 500k ms, clarion, sp=2, SYNC for VIN works in the GEN1.1 GEN2/3?? ABS does NOT work!!! RCM works too -
 config_list2.append({"filename": "oobd_control_MKT_amp.ini", "displayname": "MKT"})  # index = 10 use 500 hs,clarion, tweeters sp=2, VIN=sync, pcm and rcm if ign  is on
@@ -33,14 +33,6 @@ config_list2.append({"filename": "test.ini", "displayname": "test only"})  # ind
 
 config_file_default = config_list2[index]["filename"]  # use index above in place of the number [x]
 
-# Below are the global variables used for the modules in the project DO NOT CHANGE
-# global User_AHU_Selection
-# User_AHU_Selection = ""
-# User_Speaker_Selection = ""
-# User_AMP_Selection = False  # True or False
-# global CFT
-# CFT = config_file
-
-
+# END OF FILE END OF FILE END OF FILE END OF FILE END OF FILE END OF FILE
 
 
